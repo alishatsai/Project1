eebad221daa9116f25cef538b2ca024789816d9d
Alisha <aliciatsai919@gmail.com>
2020-07-15 07:38:16 +0800

    Initial commit

---
+++ src/main/java/Alisha/com/Person.java
@@ -0,0 +1,7 @@
+package Alisha.com;
+
+public class Person {
+    public void hello(){
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/kotlin/Alisha/com/Hello.kt
@@ -0,0 +1,15 @@
+package Alisha.com
+
+fun main() {
+//    println("Hello kotlin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+
+class  Human {
+    fun hello() {
+        println("Hello kotlin")
+    }
+}
